343b3adde44e1310c951d54cebb7951047614ecf
Cyrille CHATEAU <cyrillechateau@icloud.com>
2023-12-13 18:45:18 +0100

    Initial Commit

---
+++ Pages de Reves/Pages_de_RevesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pages_de_RevesApp.swift
+//  Pages de Reves
+//
+//  Created by Cyrille Chateau on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pages_de_RevesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
